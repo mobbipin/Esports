7a62cedd981e0c4fde278065caaddba314efef16
mobbbipin <mobbipin@mobcr3atives.com>
2023-12-16 22:46:24 +0545

    LOGIN/REGISTER AND DASHBOARD WITH JETSTREAM

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('email')->unique();
+            $table->timestamp('usertype')->default(0);
             $table->string('password');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/dashboardesports', [HomeController::class, 'index1']);
+
+
 
 Route::middleware([
     'auth:sanctum',
