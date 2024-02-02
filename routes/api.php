78f42b7da4df7b9cdfa46e67ae76a9e5eae92d42
Yakuuuuuu <121224279+Yakuuuuuu@users.noreply.github.com>
2024-02-03 05:21:56 +0545

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,27 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+class TournamentController extends Controller
+{
+    //
+}
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// app/Http/Controllers/TournamentController.php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TournamentController extends Controller
+{
+    public function saveTournament(Request $request)
+    {
+        // Validate request and save tournament data
+        // Save images and update database accordingly
+
+        return response()->json(['success' => true]);
+    }
+}
